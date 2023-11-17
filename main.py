d3395280eb03d2e9fe06e89eed00f5a32c03dab0
chenyu0516 <a0981551262@gmail.com>
2023-11-17 17:42:44 +0800

    main.py amended

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-def pdf2text(pdf_file):
+class pdf2text():
+    def __init__(self):
+        pass
+    def __call__(self, pdf_file):
+        pass
+
+
+class text2vector():
+    def __init__(self):
+        pass
+    def __call__(self, text):
+        pass
+
+
+class cosine_sim():
+    def __init__(self):
+        pass
+    def __call__(self, vector_from_table, vector_from_keyword):
+        pass
+
+def main(keyword, pdf_file):
     pass
-
-
-def text2vector(text):
-    pass
-
-
-def cosine_sim(vector):
-    pass
-
-
-def main():
-    pass
-
+    # return table
 
 if __name__ == "__main__":
     main()
