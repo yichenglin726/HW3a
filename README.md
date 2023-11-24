2f3dee5998664c21a87ef2293cdc98d408336fed
gino79445 <gino79445@gmail.com>
2023-11-24 23:46:12 +0800

    r11944078

--- README.md
+++ README.md
@@ -35,23 +35,6 @@ Search query:
 Output:
 ![image](https://github.com/Stage-A/Document-Intelligence/blob/main/images/example2.png)
 
-## How to contribute
 
-* Every one finishes the whole project and pulls the requests , do not edit the main branch
-* if your code is acceptable, we will add it into the main branch
-
-## Note
-
-* In hw3a, you can use any package you think would be helpful.
-* It's also necessary to implement the UI.
-* Also using draw io to draw a flow diagram is required.
-
-## Background Knowledge
-
-[Azure Document Intelligence]( https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence
-)
-
-## Test Document
-
-[Document 1](https://docs.google.com/document/d/1Di5oVYhUF6p-zj2y0DEBBeTvhC91KhX8/edit?usp=sharing&ouid=107784913306655694785&rtpof=true&sd=true)
-[Document 2](https://docs.google.com/document/d/1HiZrgIyvwY8Fi4eLS0QGUkkycngtD6XJ/edit?usp=sharing&ouid=107784913306655694785&rtpof=true&sd=true)
+#### Flow
+![image](./images/flow.png)
--- main.py
+++ main.py
@@ -1,43 +1,63 @@
 import camelot
-
+from sentence_transformers import SentenceTransformer
+import numpy as np
+import os
+import streamlit as st
+import pandas as pd
 
 class pdf2text:
-    def __init__(self):
-        pass
-
     def __call__(self, pdf_file):
         tables = camelot.read_pdf(pdf_file, pages="all")
         texts = []
         for table in tables:
-            texts.append(table.df.to_string())
-        text = "\n".join(texts)
-        text = text.replace("\\n", "")
-        return text
-
-
+            texts.append(table.df)
+        return texts
 class text2vector:
     def __init__(self):
-        pass
+        self.model = SentenceTransformer('bert-base-chinese')
 
     def __call__(self, text):
-        pass
-
+        return self.model.encode(text)
 
 class cosine_sim:
-    def __init__(self):
-        pass
-
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        dot_product = np.dot(vector_from_table, vector_from_keyword)
+        norm_product = np.linalg.norm(vector_from_table) * np.linalg.norm(vector_from_keyword)
+        result = dot_product / norm_product
+        return result
 
+def main():
+    st.title("PDF 表格搜索引擎")
 
-def main(keyword, pdf_file):
-    pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
+    pdf_files = [f for f in os.listdir('docs') if f.endswith('.pdf')]
+    selected_pdf = st.selectbox("選擇 PDF 文件", pdf_files)
+
+    keyword = st.text_input("輸入關鍵詞")
+
+    if selected_pdf and keyword:
+        pdf_file_path = os.path.join('docs', selected_pdf)
+        pdf_parser = pdf2text()
+        tables = pdf_parser(pdf_file_path)
+        txt2vec = text2vector()
+        keyword_vector = txt2vec(keyword)
+
+        cos_similarity = cosine_sim()
+        max_similarity = -1
+        best_table = None
+        for i, table in enumerate(tables):
+            vector_table = txt2vec(table.to_string())
+            similarity_score = cos_similarity(vector_table, keyword_vector)
+
+            st.write(f"Table {i + 1} - Cosine Similarity Score: {similarity_score}")
+
+            if similarity_score > max_similarity:
+                max_similarity = similarity_score
+                best_table = table
+
+        st.write("Best Matching Table:")
+        st.dataframe(best_table)
 
 
 if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+    main()
+
