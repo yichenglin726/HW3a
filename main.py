d7e7d7c967d1f47da7967f1e2a8d59f8d1c2ed5f
brian-konr <r12922050@ntu.edu.tw>
2023-11-24 14:26:35 +0800

    R12922050

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 import camelot
-
+import numpy as np
+import pandas as pd
+import os
+from numpy.linalg import norm
+import streamlit as st
+import random
+from utils import search_messages
+from sentence_transformers import SentenceTransformer
 
 class pdf2text:
     def __init__(self):
-        pass
-
-    def __call__(self, pdf_file):
-        tables = camelot.read_pdf(pdf_file, pages="all")
+        """
+        self.tables: list of parsed table string.
+        It is a list of dict, each dict contains two keys:
+            "df": pandas dataframe
+            "string": string of the table
+        """
+        self.tables = []
+    def __call__(self, pdf_file_path: str):
+        tables = camelot.read_pdf(pdf_file_path, pages="all")
+        print("Total tables extracted:", tables.n)
+        for table in tables:
+            self.tables.append({
+                "df": table.df,
+                "string": table.df.to_string().replace("\\n", "")
+            })
+        return self.tables
+    def print_every_table(self, pdf_file_path: str):
+        tables = camelot.read_pdf(pdf_file_path, pages="all")
         texts = []
         for table in tables:
             texts.append(table.df.to_string())
-        text = "\n".join(texts)
+        text = "\n\n".join(texts)
         text = text.replace("\\n", "")
-        return text
+        print(text)
 
 
 class text2vector:
     def __init__(self):
-        pass
+        self.model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
 
     def __call__(self, text):
-        pass
+        return self.model.encode(text)
 
 
 class cosine_sim:
@@ -28,16 +49,50 @@ class cosine_sim:
         pass
 
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        return np.dot(vector_from_table, vector_from_keyword) / (norm(vector_from_table)*norm(vector_from_keyword))
 
 
-def main(keyword, pdf_file):
+def search_best_table(keyword, pdf_file_path) -> pd.DataFrame:    
+    # parse pdf tables to string
     pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
+    tables = pdf_parser(pdf_file_path)
+    # convert string to vector
+    txt2vec = text2vector()
+    vectors = []
+    for table in tables:
+        vectors.append(txt2vec(table["string"]))
+    # calculate cosine similarity
+    cos_sim = cosine_sim()
+    cosine_scores = []
+    for vector in vectors:
+        cosine_scores.append(cos_sim(vector, txt2vec(keyword)))
+    # return table with highest similarity
+    return tables[np.argmax(cosine_scores)]["df"]
+
+def main():
+    st.title("PDF Table Search Engine")
+
+    pdf_info = {
+        '1.pdf': '監督式學習、非監督式學習、強化學習相關的資料表格',
+        '2.pdf': '動植物細胞特點、多細胞生物和單細胞生物相關的資料表格',
+        # Add more files as needed
+    }
+    for pdf, intro in pdf_info.items():
+        st.write(f"{pdf}: {intro}")
 
 
-if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+    pdf_files = [f for f in os.listdir('docs') if f.endswith('.pdf')]
+    selected_pdf = st.selectbox("Select a PDF file", pdf_files)
+
+
+    keyword = st.text_input("Enter a keyword")
+
+
+    if selected_pdf and keyword:
+        pdf_file_path = os.path.join('docs', selected_pdf)
+        with st.spinner(random.choice(search_messages)):
+            df = search_best_table(keyword, pdf_file_path)
+        st.dataframe(df)
+
+main()
+
---
+++ utils.py
@@ -0,0 +1,12 @@
+search_messages = [
+    '🔍 Finding the perfect match just for you...',
+    '🐾 Following the trail of your keyword...',
+    '🚀 Launching the search rocket...',
+    '🎈 Floating through the data clouds...',
+    '🌈 Riding the data rainbow...',
+    '🕵️‍♀️ On a secret mission to find your data...',
+    '🎁 Unwrapping the gift of data...',
+    '🔮 Gazing into the data crystal ball...',
+    '🎉 It’s a data party in here...',
+    '🍀 May the odds be ever in your keyword’s favor...'
+]
