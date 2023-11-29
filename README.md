73c7bfe025794faabceb96915ffe97e1c4e302d6
Egil Diau <egil158@gmail.com>
2023-11-29 21:29:50 +0800

    finish

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Stage-A Document Intelligence
+資工AI所, 刁一凡, branch 30
+
 
 ## Installation
 
@@ -9,6 +11,10 @@ conda install -c conda-forge ghostscript
 pip install -r requirements.txt
 ```
 
+## To Run Code
+```bash
+streamlit run main.py
+```
 ## Target
 
 Create an artificial intelligence that searches in which table in the given pdf files has the desired information.  
@@ -25,7 +31,7 @@ Create an artificial intelligence that searches in which table in the given pdf 
 #### Example
 
 The given pdf file:  
-![image](https://github.com/Stage-A/Document-Intelligence/blob/main/images/example1.png)
+![image](https://github.com/Stage-A/Document-Intelligence/blob/main/images/example1.png) 
 Search query:  
 
 ```commandline
@@ -35,23 +41,8 @@ Search query:
 Output:
 ![image](https://github.com/Stage-A/Document-Intelligence/blob/main/images/example2.png)
 
-## How to contribute
+#### Flow Diagram
+![image](./BDS_HW3.drawio.png)
 
-* Every one finishes the whole project and pulls the requests , do not edit the main branch
-* if your code is acceptable, we will add it into the main branch
-
-## Note
-
-* In hw3a, you can use any package you think would be helpful.
-* It's also necessary to implement the UI.
-* Also using draw io to draw a flow diagram is required.
-
-## Background Knowledge
-
-[Azure Document Intelligence]( https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence
-)
-
-## Test Document
-
-[Document 1](https://docs.google.com/document/d/1Di5oVYhUF6p-zj2y0DEBBeTvhC91KhX8/edit?usp=sharing&ouid=107784913306655694785&rtpof=true&sd=true)
-[Document 2](https://docs.google.com/document/d/1HiZrgIyvwY8Fi4eLS0QGUkkycngtD6XJ/edit?usp=sharing&ouid=107784913306655694785&rtpof=true&sd=true)
+#### Demo
+![image](./Demo.png)
--- main.py
+++ main.py
@@ -1,43 +1,73 @@
 import camelot
+import numpy as np
+import pandas as pd
+import os
+import streamlit as st
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
-
+# pdf to text
 class pdf2text:
     def __init__(self):
-        pass
+        self.tables = []
 
-    def __call__(self, pdf_file):
-        tables = camelot.read_pdf(pdf_file, pages="all")
-        texts = []
-        for table in tables:
-            texts.append(table.df.to_string())
-        text = "\n".join(texts)
-        text = text.replace("\\n", "")
-        return text
+    def __call__(self, pdf_file_path: str):
+        tables = camelot.read_pdf(pdf_file_path, pages="all")
+        self.tables = [{"df": table.df, "string": table.df.to_string().replace("\\n", "")} for table in tables]
+        return self.tables
 
-
+# test to vector
 class text2vector:
     def __init__(self):
-        pass
+        self.model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
 
     def __call__(self, text):
-        pass
+        return self.model.encode(text)
 
-
+# cosine similarity
 class cosine_sim:
     def __init__(self):
         pass
 
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        return cosine_similarity([vector_from_table], [vector_from_keyword])[0][0]
 
+def main():
 
-def main(keyword, pdf_file):
-    pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
+    # streamlit words
+    st.title("PDF Content Search")
+    st.markdown("1.pdf: 監督式學習、非監督式學習、強化學習相關的資料表格\n\n2.pdf: 動植物細胞特點、多細胞生物和單細胞生物相關的資料表格")
 
+    # select PDF file
+    pdf_files = [f for f in os.listdir('docs') if f.endswith('.pdf')]
+    pdf_selected = st.selectbox("Select a PDF file", pdf_files)
+
+    # input prompt bar 
+    keyword = st.text_input("Please Enter the keyword")
+
+    # result display
+    st.write("Search Result")
+
+    # Content search
+    if pdf_selected and keyword:
+
+        pdf_file_path = os.path.join('docs', pdf_selected)
+
+        # parse pdf tables to string
+        pdf_parser = pdf2text()
+        tables = pdf_parser(pdf_file_path)
+
+        # convert text to vector
+        txt2vec = text2vector()
+        vectors = [txt2vec(table["string"]) for table in tables]    
+        
+        # calculate cosine similarity
+        cos_sim = cosine_sim()
+        cosine_scores = [cos_sim(vector, txt2vec(keyword)) for vector in vectors]   
+
+        df = tables[np.argmax(cosine_scores)]["df"]
+        st.dataframe(df)
 
 if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+    main()
+
