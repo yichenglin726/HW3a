ab081fe1e355068e67e1deb687334f6f820a8066
bill <p10922004@ntu.edu.tw>
2023-12-03 21:08:31 +0800

    P10922004

--- README.md
+++ README.md
@@ -11,45 +11,59 @@ pip install -r requirements.txt
 
 ## Target
 
-Create an artificial intelligence that searches in which table in the given pdf files has the desired information.  
+Create an artificial intelligence that searching in which table in the given pdf files has the desired information.  
+
+## Usage
+
+1. Exec main.py
+
+```
+streamlit run main.py
+```
+
+2. A local web page will be displayed
 
 #### Input
 
-1. pdf files with only tables inside
-2. the searching keywords
+1. A fill-in-the-blank box called input a keyword appears below, please enter the searching keywords
 
 #### Output
 
-**the hole table with desired information in it**
+A most suitable table with desired information in it
 
-#### Example
+#### Example 1
 
 The given pdf file:  
 ![image](https://github.com/Stage-A/Document-Intelligence/blob/main/images/example1.png)
-Search query:  
+Search query:
 
-```commandline
+```input following keyword
 非監督式學習的應用
 ```
 
-Output:
+The Output is as followed:
 ![image](https://github.com/Stage-A/Document-Intelligence/blob/main/images/example2.png)
+
+#### Example 2
+The given pdf file:  
+[Document 2](https://docs.google.com/document/d/1HiZrgIyvwY8Fi4eLS0QGUkkycngtD6XJ/edit?usp=sharing&ouid=107784913306655694785&rtpof=true&sd=true)
+Search query:
+
+```input following keyword
+多細胞生物細胞膜
+```
+
+The Output is as followed:
+![image](https://github.com/Stage-A/Document-Intelligence/blob/51/images/example2out.png)
 
 ## How to contribute
 
-* Every one finishes the whole project and pulls the requests , do not edit the main branch
-* if your code is acceptable, we will add it into the main branch
-
-## Note
-
-* In hw3a, you can use any package you think would be helpful.
-* It's also necessary to implement the UI.
-* Also using draw io to draw a flow diagram is required.
+- Every one finishes the whole project and pulls the requests , do not edit the main branch
+- if your code is acceptable, we will add it into the main branch
 
 ## Background Knowledge
 
-[Azure Document Intelligence]( https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence
-)
+[Azure Document Intelligence](https://azure.microsoft.com/en-us/products/ai-services/ai-document-intelligence)
 
 ## Test Document
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import camelot
+from text2vec import SentenceModel
+from sentence_transformers import util
+import streamlit as st
 
 
 class pdf2text:
@@ -6,21 +9,41 @@ class pdf2text:
         pass
 
     def __call__(self, pdf_file):
+        # Extract tables from a PDF and retrieve titles
         tables = camelot.read_pdf(pdf_file, pages="all")
+        self.get_titles(pdf_file)
+
+        tables_text = []
+        for table, title in zip(tables, self.titles):
+            tables_text.append({"title": title, "table": table.df, "table_string": table.df.to_string().replace("\\n", "")})
+        
+        return tables_text
+    
+    def get_titles(self, pdf_file):
+        # Extract titles from a PDF
+        tables = camelot.read_pdf(pdf_file, flavor='stream', pages="all")
         texts = []
         for table in tables:
-            texts.append(table.df.to_string())
-        text = "\n".join(texts)
-        text = text.replace("\\n", "")
-        return text
+            texts.append(table.df.to_string().replace("\\n", "").strip().split(' '))
 
+        self.titles = []
+        for text in texts:
+            for word in text:
+                if "ai_tables" in word:
+                    ##print(text)
+                    title = word + " " + text[text.index(word) + 1]
+                    ##print(text[text.index(word) + 1])
+                    title = title.replace("ai_tables_", "")
+                    self.titles.append(title)
+        
 
 class text2vector:
     def __init__(self):
-        pass
+        self.model = SentenceModel('shibing624/text2vec-base-chinese')
 
     def __call__(self, text):
-        pass
+        vector = self.model.encode(text)
+        return vector
 
 
 class cosine_sim:
@@ -28,16 +51,92 @@ class cosine_sim:
         pass
 
     def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+        # Calculate cosine similarity between two vectors
+        cos_sim_val = util.pytorch_cos_sim(vector_from_table, vector_from_keyword)
+        # return util.pytorch_cos_sim(vector_from_table, vector_from_keyword)
+        return cos_sim_val
 
 
-def main(keyword, pdf_file):
+def main(keyword, pdf_files):
     pdf_parser = pdf2text()
-    table_text = pdf_parser(pdf_file)
-    print(table_text)
-    # return table
+    max_sim = 0
+    max_sim_table = None
+    max_sim_title = None
+    for pdf_file in pdf_files:
+        tables_text = pdf_parser(pdf_file)
+        text2vector_fn = text2vector()
+
+        for table_text in tables_text:
+            table_string = table_text["table_string"]
+            table_vector = text2vector_fn(table_string)
+            keyword_vector = text2vector_fn(keyword)
+            similarity = cosine_sim()(table_vector, keyword_vector)
+            table_text["similarity"] = similarity
+    
+        # Find the most similar table by table_text["similarity"]
+        for table_text in tables_text:
+        #print(table_text["title"])
+        #print(table_text["table"])
+        #print(table_text["similarity"])
+            if table_text["similarity"] > max_sim:
+                max_sim = table_text["similarity"]
+                max_sim_table = table_text["table"]
+                max_sim_title = table_text["title"]
+    
+    return max_sim_table, max_sim_title
+
 
 
 if __name__ == "__main__":
-    main("keyword", "docs/1.pdf")
-    main("keyword", "docs/2.pdf")
+    # Configure Streamlit page settings
+    st.set_page_config(
+        page_title="AI Table Search Agent",
+        page_icon="random",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        )
+    max_sim_table = None
+    max_sim_title = None
+    # Display the application title and examples
+    st.title("AI Table Search Agent")
+    st.write("Example 1: 監督式學習、非監督式學習、強化學習")
+    st.write("Example 2: 動物細胞、植物細胞、多細胞生物、單細胞生物、多細胞生物細胞膜、植物細胞膜")
+
+    # Define the list of PDF files
+    pdf_files = ["docs/1.pdf", "docs/2.pdf"]
+    #print(pdf_files)
+
+    # Display the input keyword field
+    st.subheader("Input a keyword")
+    print('submit status:', st.session_state)
+    keyword=''
+    keyword = st.text_input("Key in the fill-in-the-blank box with Keyword")
+    submit_button = st.button("Search")
+    #if "submit_button_state" not in st.session_state:
+     #   st.session_state.submit_button_state = False
+
+    #if submit_button or st.session_state.submit_button_state:
+        #st.session_state.submit_button_state = True
+
+    #if submit_button or keyword:
+
+    if "submit_button_state" not in st.session_state:
+        st.session_state.submit_button_state = False
+    if submit_button or st.session_state.submit_button_state:
+        st.session_state.submit_button_state = True
+        #if submit_button or (keyword and pdf_files):
+        print(keyword)
+        print(pdf_files)
+        print(submit_button)
+        #submit_button = False
+        with st.spinner('Searching the most suitable table'):
+        #table = main(keyword, pdf_file)
+        #st.write("Output:", table)
+            max_sim_table, max_sim_title = main(keyword, pdf_files)
+            # show the most similar table 
+            st.subheader(max_sim_title)
+            st.write(max_sim_table)
+        #if "submit_button_state" not in st.session_state:
+            st.session_state.submit_button_state = False
+
+
